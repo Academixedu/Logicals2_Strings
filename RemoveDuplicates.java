b5f5884f91b9bc25095e967e6021c547287aa870
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-10 12:36:09 +0530

    Create RemoveDuplicates.java

---
+++ RemoveDuplicates.java
@@ -0,0 +1,25 @@
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        String input = "Hello World";  
+        String result = removeDuplicates(input); 
+        System.out.println("Original String: " + input);
+        System.out.println("String after removing duplicates: " + result);
+    }
+
+    public static String removeDuplicates(String str) {
+        String result = ""; 
+
+    
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+          
+            if (result.indexOf(c) == -1) {
+                result += c; 
+            }
+        }
+
+        return result; 
+    }
+}
